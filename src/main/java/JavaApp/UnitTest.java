faa7a750d79fffbe5138b9030996c07e13c67dc0
Spencer Wieczorek <spencerwie@gmail.com>
2017-12-16 02:15:41 -0500

    Final Unit Test
    Final Unit Test for application are compelte.

--- src/main/java/JavaApp/UnitTest.java
+++ src/main/java/JavaApp/UnitTest.java
@@ -9,14 +9,11 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.google.gson.Gson;
-import com.nimbusds.oauth2.sdk.http.HTTPRequest;
-
 import static org.assertj.core.api.Assertions.assertThat;
 /*
  *  Important: Tests depend on the default single student "John Smith" defined in DatabaseLoader.
